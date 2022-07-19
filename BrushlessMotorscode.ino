84189619b981ee30faec8289f1605d8825a93319
Roa Mohamed <70070721+RoaPro9@users.noreply.github.com>
2022-07-19 09:05:54 +0300

    Add files via upload

---
+++ BrushlessMotorscode.ino
@@ -0,0 +1,26 @@
+// C++ code
+//
+
+
+int motorPin = 10;
+ 
+void setup() 
+{ 
+  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+  while (! Serial);
+  Serial.println("Speed 0 to 255");
+} 
+ 
+ 
+void loop() 
+{ 
+  if (Serial.available())
+  {
+    int speed = Serial.parseInt();
+    if (speed >= 0 && speed <= 255)
+    {
+      analogWrite(motorPin, speed);
+    }
+  }
+} 
